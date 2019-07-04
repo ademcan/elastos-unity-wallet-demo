0630ea3f62caa3d78f7069b0d9e0109b3d3bd397
ademcan <ademcan@ademcan.net>
2019-07-04 09:40:01 +0200

    Add Detox tests to Android

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,16 +1,22 @@
 // import console = require("console");
 
-describe('Example', () => {
+describe('Elastos Unity Wallet', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
+  it('should return a correct mnemonic', async () => {
     await expect(element(by.id('btn:GenerateMnemonic'))).toBeVisible();
     await element(by.id('btn:GenerateMnemonic')).tap();
     await expect(element(by.id('txt:Result'))).toHaveText('success');
     // let test = await element(by.id('txt:Result'))
     // console.log( test )
+  });
+
+  it('should create a wallet when mnemonic is provided', async () => {
+    await expect(element(by.id('btn:CreateWallet'))).toBeVisible();
+    await element(by.id('btn:CreateWallet')).tap();
+    await expect(element(by.id('txt:Result'))).toHaveText('success');
   });
 
   // it('should show hello screen after tap', async () => {
